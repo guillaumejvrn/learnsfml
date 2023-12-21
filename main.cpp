89125a6a52efe41f78dfb634f56b4434885a8aea
guillaumejvrn <guillaume.jenvrin@sts-sio-caen.info>
2023-12-21 16:36:52 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow fenetre(sf::VideoMode(800, 600), "Ma Fenêtre SFML");
+
+    while (fenetre.isOpen()) {
+        sf::Event evenement;
+        while (fenetre.pollEvent(evenement)) {
+            if (evenement.type == sf::Event::Closed) {
+                fenetre.close();
+            }
+        }
+
+        fenetre.clear();
+        // Dessinez ici
+        fenetre.display();
+    }
+
+    return 0;
+}
